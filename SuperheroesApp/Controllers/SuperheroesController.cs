c0d31c7b2cd9fa9abe76a81adbf82b301a881f99
ChaseLoeffler <chaselo609@gmail.com>
2023-09-08 11:52:55 -0400

    Added code for SuperheroesContoller methods Index,Details,create, and edit.

--- SuperheroesApp/Controllers/SuperheroesController.cs
+++ SuperheroesApp/Controllers/SuperheroesController.cs
@@ -1,21 +1,33 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperheroesApp.Data;
 using SuperheroesApp.Models;
 
 namespace SuperheroesApp.Controllers
 {
     public class SuperheroesController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public SuperheroesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         // GET: SuperheroesController
+
         public ActionResult Index()
         {
-            return View();
+            var heroes = _context.Superheroes;
+            return View(heroes);
         }
 
         // GET: SuperheroesController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var heroDetails = _context.Superheroes.Find(id);
+            return View(heroDetails);
         }
 
         // GET: SuperheroesController/Create
@@ -29,6 +41,8 @@ namespace SuperheroesApp.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(SuperHero hero)
         {
+            _context.Superheroes.Add(hero);
+            _context.SaveChanges();
             try
             {
                 return RedirectToAction(nameof(Index));
@@ -42,7 +56,8 @@ namespace SuperheroesApp.Controllers
         // GET: SuperheroesController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var hero = _context.Superheroes.Find(id);
+            return View(hero);
         }
 
         // POST: SuperheroesController/Edit/5
@@ -50,6 +65,7 @@ namespace SuperheroesApp.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, SuperHero hero)
         {
+
             try
             {
                 return RedirectToAction(nameof(Index));
@@ -63,7 +79,8 @@ namespace SuperheroesApp.Controllers
         // GET: SuperheroesController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var hero = _context.Superheroes.Find(id);
+            return View(hero);
         }
 
         // POST: SuperheroesController/Delete/5
